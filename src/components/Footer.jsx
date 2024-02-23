9691afaa252527403411f7ef02d07ec5d18f2c5e
zaputlah <reza.rr889@gmail.com>
2024-02-23 17:44:33 +0700

    img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,29 +4,29 @@ const Footer = () => {
             <p className="text-center mb-6">Supported by:</p>
             <div className="flex overflow-hidden space-x-16 group">
                 <div className="flex space-x-16 animate-loop-scroll group-hover:paused">
-                    <img loading="lazy" src="../../public/img/netflix.png" alt="Netflix" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/youtube.png" alt="YouTube" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/remini.png" alt="Remini" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/picsart.png" alt="Picsart" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/Iqiyi.png" alt="IQiyi" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/viu.webp" alt="Viu" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/Capcut.png" alt="Capcut" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/Spotify.png" alt="Spotify" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/VSCO.png" alt="Vsco" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/WeTV.png" alt="WeTv" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/Vidio.png" alt="Vidio" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/ampro.png" alt="Ampro" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/dazzcam.webp" alt="Dazzcam" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/tiktokmusic.png" alt="TiktokMusic" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/google_drive.png" alt="GoogleDrive" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/HBOGO.png" alt="Hbo Go" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/get-contact.png" alt="Get Contact" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/youku.png" alt="YouKu" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/msword.png" alt="Ms Word" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/canva.webp" alt="Canva" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/applemusic.png" alt="AppleMusic" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/iCloud.png" alt="ICloud" className="mx-w-none h-10" />
-                    <img loading="lazy" src="../../public/img/Disney.png" alt="Disney" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/netflix.png" alt="Netflix" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/youtube.png" alt="YouTube" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/remini.png" alt="Remini" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/picsart.png" alt="Picsart" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/Iqiyi.png" alt="IQiyi" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/viu.webp" alt="Viu" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/Capcut.png" alt="Capcut" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/Spotify.png" alt="Spotify" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/VSCO.png" alt="Vsco" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/WeTV.png" alt="WeTv" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/Vidio.png" alt="Vidio" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/ampro.png" alt="Ampro" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/dazzcam.webp" alt="Dazzcam" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/tiktokmusic.png" alt="TiktokMusic" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/google_drive.png" alt="GoogleDrive" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/HBOGO.png" alt="Hbo Go" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/get-contact.png" alt="Get Contact" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/youku.png" alt="YouKu" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/msword.png" alt="Ms Word" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/canva.webp" alt="Canva" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/applemusic.png" alt="AppleMusic" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/iCloud.png" alt="ICloud" className="mx-w-none h-10" />
+                    <img loading="lazy" src="../img/Disney.png" alt="Disney" className="mx-w-none h-10" />
                 </div>
                 {/* <div className="flex space-x-16 animate-loop-scroll group-hover:paused" aria-hidden="true">
                     <img loading="lazy" src="../../public/img/netflix.png" alt="Netflix" className="mx-w-none h-10" />
@@ -56,11 +56,11 @@ const Footer = () => {
             </div>
             <p className="text-center mb-4">Payment:</p>
             <div className="flex space-x-16 justify-center items-center">
-                <img src="../../public/img/Gopay.png" alt="Payment Methods" className="mx-w-none h-10" />
-                <img src="../../public/img/dana.png" alt="Payment Methods" className="mx-w-none h-10" />
-                <img src="../../public/img/ovo.png" alt="Payment Methods" className="mx-w-none h-10" />
-                <img src="../../public/img/bri.png" alt="Payment Methods" className="mx-w-none h-10" />
-                <img src="../../public/img/bca.webp" alt="Payment Methods" className="mx-w-none h-10" />
+                <img src="../img/Gopay.png" alt="Payment Methods" className="mx-w-none h-10" />
+                <img src="../img/dana.png" alt="Payment Methods" className="mx-w-none h-10" />
+                <img src="../img/ovo.png" alt="Payment Methods" className="mx-w-none h-10" />
+                <img src="../img/bri.png" alt="Payment Methods" className="mx-w-none h-10" />
+                <img src="../img/bca.webp" alt="Payment Methods" className="mx-w-none h-10" />
             </div>
                 <div className="flex flex-wrap justify-center items-center">
     <div className="flex items-center mt-5">
